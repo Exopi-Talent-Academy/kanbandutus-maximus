3f8659997e874dacb0b4e118aa63ced6b909f620
Bhuwan Krishna Som Poudel <bkspoudel@gmail.com>
2026-04-14 15:24:49 +0200

    implement edit and delete task functionality

---
+++ frontend/src/app/components/task-form/task-form.ts
@@ -0,0 +1,32 @@
+import { Component, inject, input, Input, OnInit, signal } from '@angular/core';
+import { Container } from '../../shared/container/container';
+import { FormsModule } from '@angular/forms';
+import { Task, TaskType } from '../../models/types';
+import { Tasks } from '../../services/tasks';
+
+@Component({
+  selector: 'app-task-form',
+  imports: [Container, FormsModule],
+  templateUrl: './task-form.html',
+  styleUrl: './task-form.css',
+})
+export class TaskForm implements OnInit {
+  @Input() currentTask!: Task;
+
+  taskService = inject(Tasks);
+
+  ngOnInit() {
+    console.log('Task received in TaskForm component:', this.currentTask);
+  }
+  deleteTask() {
+    this.taskService.deleteTask(this.currentTask.id).subscribe(() => {
+      this.taskService.cancelEditTask();
+    });
+  }
+
+  saveTask() {
+    this.taskService.updateTask(this.currentTask).subscribe(() => {
+      this.taskService.cancelEditTask();
+    });
+  }
+}
