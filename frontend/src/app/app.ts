a9a0ec8ee08b82aeec0b7d4643347586f3f253ed
Bhuwan Krishna Som Poudel <bkspoudel@gmail.com>
2026-04-14 15:24:49 +0200

    implement edit and delete task functionality

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -5,15 +5,19 @@ import { Header } from './components/header/header';
 import { NavBar } from './components/nav-bar/nav-bar';
 import { Board } from './components/board/board';
 import { Tasks } from './services/tasks';
+import { Task } from './models/types';
+import { TaskForm } from './components/task-form/task-form';
 
 @Component({
   selector: 'app-root',
-  imports: [Container, Header, NavBar, Board],
+  imports: [Container, Header, NavBar, Board, TaskForm],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App implements OnInit {
   protected readonly title = signal('kanban');
+
+  task = signal<Task | null>(null);
 
   isEditable = signal(false);
 
@@ -21,10 +25,13 @@ export class App implements OnInit {
 
   ngOnInit() {
     this.taskService.editTask.subscribe((task) => {
+      console.log('Received task for editing:', task);
       if (task) {
         this.isEditable.set(true);
+        this.task.set(task);
       } else {
         this.isEditable.set(false);
+        this.task.set(null);
       }
     });
   }
