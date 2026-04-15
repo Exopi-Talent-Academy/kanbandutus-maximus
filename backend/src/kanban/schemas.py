541e0fb115b22093eec134913987162f8eb47df3
Michaelius A'Chapelo <scaveleon@gmail.com>
2026-04-15 11:28:31 +0200

    Added SQL Schema

---
+++ backend/src/kanban/schemas.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Column, ForeignKey, Integer, MetaData, String, Table
+
+# https://docs.sqlalchemy.org/en/20/core/metadata.html#accessing-tables-and-columns
+
+metadata_obj = MetaData()
+
+board = Table(
+    "board",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("name", String(50), nullable=False),
+)
+
+
+columns = Table(
+    "columns",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("name", String(50), nullable=False),
+    Column("position", Integer, nullable=False),
+    Column("board_id", Integer, ForeignKey("board.id", ondelete="CASCADE"), nullable=False, index=True),
+)
+
+tasks = Table(
+    "tasks",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("column_id", Integer, ForeignKey("columns.id", ondelete="CASCADE"), nullable=False, index=True),
+    Column("title", String(50), nullable=False),
+    Column("description", String(200), nullable=False),
+    Column("position", Integer, nullable=False),
+)
