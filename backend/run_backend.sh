2dccbb2043c3bc5460a99c84d5e84f1372b3c031
Uffe Wassmann <silverfoil@arcticmail.com>
2026-04-15 20:06:08 +0200

    backend - script improvement
    Added check if virtual environment already exists
    before attempting to create it.
    
    Had an error on 'required file not found'.
    This happens when Windows' line endings
    are present, which bash gets confused about.
    
    If git reintroduces Windows' line endings,
    the git configuration may need to be reconsidered.

--- backend/run_backend.sh
+++ backend/run_backend.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
-
-python3 -m venv virtual-environment-kanban
+#!/usr/bin/bash
+if [ ! -d "virtual-environment-kanban" ]; then
+    python3 -m venv virtual-environment-kanban
+fi
 source virtual-environment-kanban/bin/activate
 pip install -e ".[dev]"
 uvicorn src.kanban.main:app --reload
