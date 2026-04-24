f215a7bc62c185b552520d66e885c4579a01b56e
Bhuwan Krishna Som Poudel <bkspoudel@gmail.com>
2026-04-24 20:02:01 +0200

    do not allow add new task and edit field for guest

--- frontend/src/app/components/add-new-task/add-new-task.ts
+++ frontend/src/app/components/add-new-task/add-new-task.ts
@@ -3,10 +3,11 @@ import { Container } from '../../shared/container/container';
 import { Tasks } from '../../services/tasks';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faAdd } from '@fortawesome/free-solid-svg-icons';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-add-new-task',
-  imports: [Container, FontAwesomeModule],
+  imports: [Container, FontAwesomeModule, NgClass],
   templateUrl: './add-new-task.html',
   styleUrl: './add-new-task.css',
 })
@@ -16,14 +17,22 @@ export class AddNewTask {
   taskService = inject(Tasks);
 
   addNewTask() {
-    this.taskService.onEditTask({
-      id: 0,
-      title: '',
-      column_id: this.columnId,
-      assignee: '',
-      description: '',
-      position: 1110,
-    });
-    this.taskService.togggleAddTaskMode(true);
+    if (this.getUserId() === '4') {
+      return;
+    } else {
+      this.taskService.onEditTask({
+        id: 0,
+        title: '',
+        column_id: this.columnId,
+        assignee: '',
+        description: '',
+        position: 1110,
+      });
+      this.taskService.togggleAddTaskMode(true);
+    }
+  }
+
+  getUserId() {
+    return localStorage.getItem('userId');
   }
 }
